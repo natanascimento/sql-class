42886ad96cfa48e0f9de1c4ea08609692af3f928
Natan Nascimento <natan.nascimento@xpi.com.br>
2022-02-27 20:04:42 -0300

    feat: added terraform provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,13 @@
+#Realizando uma requisição da hashicorp
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "google" {
 credentials = "${file("credentials.json")}"
 project = "ac-shared-playground"
