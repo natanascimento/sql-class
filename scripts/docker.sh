5282496fa770cd9a55cb26b9207f691d30d6e46f
Natan Nascimento <natan.nascimento@xpi.com.br>
2022-02-27 21:30:36 -0300

    feat: added new methods to install docker

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,10 +1,10 @@
-!/bin/bash
-sudo apt-get update
+#!/bin/bash
+sudo apt update
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu `lsb_release -cs` test"
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo apt update
+sudo apt install -y docker-ce
 echo "Docker was installed!"
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
