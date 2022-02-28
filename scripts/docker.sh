a9e2d79b882eb78e702a03986453efec1411c922
Natan Nascimento <natan.nascimento@xpi.com.br>
2022-02-27 22:08:07 -0300

    chore: update scripts and vm-instance (#6)

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-sudo apt update
-sudo apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-sudo apt update
-apt-cache policy docker-ce
-sudo apt install -y docker-ce
+sudo apt-get update
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu `lsb_release -cs` test"
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 echo "Docker was installed!"
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
